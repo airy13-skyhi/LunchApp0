a320270f72a1044dee9056bcef30572766ce8890
github airy13-skyhi <airy13@icloud.com>
2021-04-15 10:18:20 +0900

    TabBarController追加

---
+++ LunchApp0/Controller/TabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarController.swift
+//  LunchApp0
+//
+//  Created by Manabu Kuramochi on 2021/04/15.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
